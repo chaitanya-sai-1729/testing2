78011082dfc1b7b0e2cdf4c7fe884656d85c9b21
Chaitanya <sivabodicherla986@gmail.com>
2023-03-24 09:30:32 +0530

    after product

--- index.js
+++ index.js
@@ -40,6 +40,60 @@ app.get("/stock",async(req,res)=>{
     
 })
 
+app.post("/stage1",async(req,res)=>{
+    const _id=req.body.rfid;
+
+    const collection = client.db().collection("product");
+
+    try{
+        await collection.insertOne({_id:_id,stage_1:1,stage_2:0,stage_3:0});
+        res.send("Succesfully posted");
+    }catch(e){
+        console.log(e);
+    }
+
+    
+})
+
+app.post("/stage2",async(req,res)=>{
+    const rfid = req.body.rfid;
+
+    const collection = client.db().collection("product");
+
+    try{
+        await collection.updateOne({ _id:rfid }, { $set: { stage_1:0, stage_2:1,stage_3:0 } });
+        res.send("Succesfully posted");
+    }catch(e){
+        console.log(e);
+    }
+})
+
+app.post("/stage3",async(req,res)=>{
+    const rfid = req.body.rfid;
+
+    const collection = client.db().collection("product");
+
+    try{
+        await collection.updateOne({ _id:rfid }, { $set: { stage_1:0, stage_2:0,stage_3:1 } });
+        res.send("Succesfully posted");
+    }catch(e){
+        console.log(e);
+    }
+})
+
+app.get("/number",async(req,res)=>{
+    const collection = client.db().collection("product");
+
+    try{
+        const array1=await collection.find({stage_1:1}).toArray();
+        const array2=await collection.find({stage_2:1}).toArray();
+        const array3=await collection.find({stage_3:1}).toArray();
+
+        res.json({stage_1:array1.length,stage_2:array2.length,stage_3:array3.length});
+    }catch(e){
+        console.log(e);
+    }
+})
 
 
 app.listen(3000,()=>{
